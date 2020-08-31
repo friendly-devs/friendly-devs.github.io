f4269027a3b211b29e752c840c2c705f07deabd4
Võ Văn Tâm <vovantam.dev@gmail.com>
2020-08-31 15:53:39 +0000

    Publishing github pages [skip ci]

---
+++ precache-manifest.da59f5715d6a339792e5725db24dd3af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "722abacc049cb7ba5c68fb095ec22063",
+    "url": "/index.html"
+  },
+  {
+    "revision": "531cf8ef373e3608fbea",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ed37446dc28b0ff6c7e0",
+    "url": "/static/js/2.59b24aad.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.59b24aad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "531cf8ef373e3608fbea",
+    "url": "/static/js/main.c36347de.chunk.js"
+  },
+  {
+    "revision": "cdffa0d6cca93fc73681",
+    "url": "/static/js/runtime-main.d7771c8e.js"
+  }
+]);
