6f7b7a3d0a56684aeb8c57b1900145b3c5092d5b
Vo Van Tam <vovantam.dev@gmail.com>
2020-08-31 10:05:43 +0700

    Init

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+
+#!/bin/sh
+
+# Config username and email
+git config --global user.email "vovantam.dev@gmail.com"
+git config --global user.name "Võ Văn Tâm"
+
+# Checkout new branch
+git checkout -b gh-pages
+
+# Build static pages
+npm run build
+
+# Remote git
+rm -rf .git
+cd build
+
+# Init git
+git init
+git add remote add origin git@github.com:friendly-devs/friendly-devs.github.io.git
+
+# Push to github repository
+git add .
+git commit -m "Publishing github pages [skip ci]"
+git push origin gh-pages -f
